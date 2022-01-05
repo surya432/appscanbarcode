55fbda213194195f388ff5bcef9980061560263b
surya432 <hadisurya295@gmail.com>
2022-01-05 09:18:44 +0700

    modif bugs

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import {
   StatusBar,
   StyleSheet,
   Text,
+  Vibration,
   View,
   TextInput,
   FlatList,
@@ -12,12 +13,11 @@ import {
 } from 'react-native';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import {RNCamera} from 'react-native-camera';
-import glugSfx from './beep.mp3';
 import Sound from 'react-native-sound';
 const App = () => {
+  const ONE_SECOND_IN_MS = 1000;
   const [isCamera, setIsCamera] = useState(false);
-  const [input, setInput] = useState('0000000521357002140580');
-  const toggleSwitch = () => setIsCamera(!isCamera);
+  const [input, setInput] = useState(''); //0000000521357002140580
   const [listGWNW, setListGWNW] = useState([]);
   const [grandGW, setGrandGW] = useState(0);
   const [grandNW, setGrandNW] = useState(0);
@@ -30,6 +30,13 @@ const App = () => {
       }
     }),
   );
+  const soundRefError = useRef(
+    new Sound(require('./error.mp3'), Sound.MAIN_BUNDLE, error => {
+      if (error) {
+        console.error('error', error);
+      }
+    }),
+  );
 
   const prosesInput = () => {
     if (input.length == 22 && !isNaN(input)) {
@@ -37,6 +44,8 @@ const App = () => {
       if (listGWNW.length > 0) {
         listGWNW.forEach(item => {
           if (item.sn == input) {
+            Vibration.vibrate(3 * ONE_SECOND_IN_MS);
+            soundRefError.current.play();
             Alert.alert('Double Scan!!', 'Silakan scan Barcode lain', [
               {
                 text: 'ok',
@@ -46,13 +55,13 @@ const App = () => {
                 },
               },
             ]);
-            setInput('');
             double = true;
             return;
           }
         });
       }
       if (!double) {
+        soundRef.current.play();
         let gwInput = input.substring(18, 24) / 100;
         let nwInput = input.substring(14, 18) / 100;
         let sumGWNW = gwInput + nwInput;
@@ -73,15 +82,12 @@ const App = () => {
         setGrandGW(parseFloat(dataGW).toFixed(2));
         setGrandNW(parseFloat(dataNW).toFixed(2));
         setListGWNW(listData);
+        setInput('');
         if (!isCamera) {
           inputRef.current.focus();
         }
       }
     }
-    setInput('');
-  };
-  const onBarCodeRead = e => {
-    setInput(e.data);
   };
   const clearData = () => {
     Alert.alert('Menghapus data.', 'apa anda ingin menghapus data scan?', [
@@ -168,7 +174,6 @@ const App = () => {
                 }}
                 onBarCodeRead={async e => {
                   if (barcode != e.data) {
-                    soundRef.current.play();
                     setBarcode(e.data);
                     setInput(e.data);
                   }
